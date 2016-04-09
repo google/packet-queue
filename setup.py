0195b53d534840f8190038d8920bc8f1b9fb892d
Minke Zhang <minkezhang@google.com>
2016-04-09 11:06:30 -0700

    edit docstring setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from os import path
 setup(
     name='packet_queue',
     version='0.1.0',
-    zip_safe=False,  # https://github.com/ldx/python-iptables/issues/76
+    zip_safe=False,  # python-iptables doesn't work well wtih eggs
 
     description='Packet-based impaired network library',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
